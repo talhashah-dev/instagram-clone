506494817b9e47edae774aaca6e3e470492fff3c
Syed Talha <syedt.in00@gmail.com>
2024-09-14 20:14:25 +0500

    added icons to sidebar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,16 @@
 import Link from "next/link";
 import React from "react";
 import Image from "next/image";
+import {
+  AiFillHome,
+  AiOutlineCompass,
+  AiOutlineMessage,
+  AiOutlineUser,
+  AiOutlinePlus,
+  AiOutlineSearch,
+  AiOutlineMenu,
+} from "react-icons/ai";
+import { MdOutlineMovieCreation } from "react-icons/md";
 
 const Navbar = () => {
   const links = [
@@ -9,29 +19,34 @@ const Navbar = () => {
       title: "Home",
       path: "/",
       mobileHide: false,
+      icon: <AiFillHome size={"28px"} />,
     },
     {
       id: 3,
       title: "Explore",
       path: "/explore",
       mobileHide: false,
+      icon: <AiOutlineCompass size={"28px"} />,
     },
     {
       id: 4,
       title: "Reels",
       path: "/reels",
       mobileHide: false,
+      icon: <MdOutlineMovieCreation size={"28px"} />,
     },
     {
       id: 5,
       title: "Messages",
       path: "/messages",
       mobileHide: true,
+      icon: <AiOutlineMessage size={"28px"} />,
     },
     {
       id: 7,
       title: "Profile",
       path: "/profile",
+      icon: <AiOutlineUser size={"28px"} />,
     },
   ];
 
@@ -41,32 +56,35 @@ const Navbar = () => {
       title: "Create",
       act: () => alert("create function"),
       mobileHide: false,
+      icon: <AiOutlinePlus size={"22px"} />,
     },
     {
       id: 2,
       title: "Search",
       act: () => alert("search function"),
       mobileHide: true,
+      icon: <AiOutlineSearch size={"22px"} />,
     },
     {
       id: 3,
       title: "Menu",
       act: () => alert("menu modal"),
       mobileHide: true,
+      icon: <AiOutlineMenu size={"22px"} />,
     },
   ];
 
-  
   return (
-    <nav className="max-sm:overflow-x-hidden">
-      <ul className="flex flex-col max-sm:flex-row gap-5 justify-between h-full">
+    <nav className="max-sm:overflow-x-hidden max-sm:w-full">
+      <ul className="flex flex-col max-sm:flex-row gap-5 max-sm:gap-2 justify-around h-full">
         {links.map((v) => {
           return (
             <li
               key={v.id}
-              className="text-white p-2 rounded-md hover:bg-[#212121]"
+              className="text-white p-2 rounded-md hover:bg-[#212121] flex gap-6 items-center"
             >
-              <Link href={v.path} className="h-full w-full">
+              {v.icon}
+              <Link href={v.path} className="h-full w-full max-md:hidden">
                 {v.title}
               </Link>
             </li>
@@ -76,9 +94,10 @@ const Navbar = () => {
           return (
             <button
               key={v.id}
-              className="text-white p-2 rounded-md hover:bg-[#212121] text-left max-sm:hidden"
+              className="text-white p-2 rounded-md hover:bg-[#212121] text-left max-md:hidden flex gap-6 items-center"
               onClick={v.act}
             >
+              {v.icon}
               {v.title}
             </button>
           );
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,12 +2,14 @@ import Link from "next/link";
 import React from "react";
 import Navbar from "./Navbar";
 import Image from "next/image";
+import { AiFillInstagram } from "react-icons/ai";
 
 const Sidebar = () => {
   return (
     <div className="bg-black w-[18%] h-screen px-5 py-5 flex flex-col gap-5 border-gray-700 border-r-[1px] max-sm:border-t-[1px] max-xl:w-[10%] relative max-sm:w-screen max-sm:h-auto max-sm:flex-row max-sm:justify-evenly">
       <Link className="text-white text-4xl p-2 heading max-sm:hidden" href={"/"}>
         Instagram
+        <AiFillInstagram size={"36px"} className="hidden" />
       </Link>
       <Navbar />
     </div>
