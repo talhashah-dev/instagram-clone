8354369c00e4c89643c27df8d88c510ffb666b23
Syed Talha <syedt.in00@gmail.com>
2024-09-14 17:00:41 +0500

    sidebar added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,41 +1,73 @@
 import Link from "next/link";
 import React from "react";
+import Image from "next/image";
 
 const Navbar = () => {
   const links = [
     {
       id: 1,
-      title: "Explore",
-      path: "/explore",
-    },
-    {
-      id: 2,
-      title: "Messages",
-      path: "/messages",
+      title: "Home",
+      path: "/",
     },
     {
       id: 3,
-      title: "Profile",
-      path: "/profile",
+      title: "Explore",
+      path: "/explore",
     },
     {
       id: 4,
       title: "Reels",
       path: "/reels",
     },
+    {
+      id: 5,
+      title: "Messages",
+      path: "/messages",
+    },
+    {
+      id: 7,
+      title: "Profile",
+      path: "/profile",
+    }
+  ];
+
+  const actionBtns = [
+    {
+      id: 1,
+      title: "Create",
+      act: () => alert("create function")
+    },
+    {
+      id: 2,
+      title: "Search",
+      act: () => alert("search function")
+    },
+    {
+      id: 3,
+      title: "Menu",
+      act: () => alert("menu modal")
+    }
   ];
   return (
     <nav className="">
       <ul className="flex flex-col gap-5 justify-between h-full">
         {links.map((v) => {
           return (
-            <li key={v.id} className="text-white text-lg p-2 rounded-md hover:bg-[#212121]" >
-              <Link href={v.path} className="inline-block h-full w-full">
+            <li
+              key={v.id}
+              className="text-white p-2 rounded-md hover:bg-[#212121] flex items-center gap-3"
+            >
+              <Link href={v.path} className="h-full w-full max-xl:hidden" >
                 {v.title}
               </Link>
             </li>
           );
         })}
+        {actionBtns.map(v => {
+        return (
+          <button key={v.id} className="text-white p-2 rounded-md hover:bg-[#212121] max-xl:hidden text-left" onClick={v.act}>{v.title}</button>
+        )
+      })}
       </ul>
     </nav>
   );
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,11 +1,12 @@
 import Link from "next/link";
 import React from "react";
 import Navbar from "./Navbar";
+import Image from "next/image";
 
 const Sidebar = () => {
   return (
-    <div className="bg-black w-[15%] h-screen px-5 py-5 flex flex-col gap-5 border-r-gray-700 border-r-[1px]">
-      <Link className="text-white text-xl p-2 heading" href={"/"}>
+    <div className="bg-black w-[18%] h-screen px-5 py-5 flex flex-col gap-5 border-r-gray-700 border-r-[1px] max-xl:w-[10%] relative">
+      <Link className="text-white text-4xl p-2 heading max-lg:hidden" href={"/"}>
         Instagram
       </Link>
       <Navbar />
